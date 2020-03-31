a04fc9a5cd7e6359598b11b5f553b7a6676274ff
krisnaa-dev <krisnaamkr@gmail.com>
2020-03-31 10:26:47 -0500

    new change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello world ! from github.com");
+        System.out.println("hello world");
 
         System.out.println("Krisnaa");
     }
